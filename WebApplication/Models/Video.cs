aabb8ddfbbf38fd660f668d59cfa40e2c381b853
grandiloquent <1785907865@qq.com>
2019-02-22 20:30:30 +0800

    changed

--- WebApplication/Models/Video.cs
+++ WebApplication/Models/Video.cs
@@ -1,33 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
-using WebApplication.Binders;
-using WebApplication.Shared;
-
 namespace WebApplication.Models
 {
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using WebApplication.Binders;
+    using WebApplication.Shared;
+
     public class Video
     {
-        public long Id { get; set; }
-        [Required] [StringLength(100)] public string Title { get; set; }
-
-        [Required] [StringLength(512)] public string Url { get; set; }
-        [Required] [StringLength(100)] public string Album { get; set; }
         public string Cover { get; set; }
-        public string Thumbnail { get; set; }
-        public int WatchedCount { get; set; }
-        public int VoteUp { get; set; }
-        public int VoteDown { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
         public string Duration { get; set; }
+        public Album Album { get; set; }
+        public int Height { get; set; }
+        public long Id { get; set; }
 
         [BindProperty(BinderType = typeof(SplitModelBinder))]
         public List<string> Tags { get; set; }
 
+        public string Thumbnail { get; set; }
+        [Required] [StringLength(100)] public string Title { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        [Required] [StringLength(512)] public string Url { get; set; }
+        public int VoteDown { get; set; }
+        public int VoteUp { get; set; }
+        public int WatchedCount { get; set; }
         public int Width { get; set; }
-        public int Height { get; set; }
 
         public override string ToString()
         {
